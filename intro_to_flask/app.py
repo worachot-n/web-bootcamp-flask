383924c0c4c55ed73ab3627c91a0197337f4cc96
ball-wch <worachotn@gmail.com>
2023-01-08 15:23:59 +0700

    update jinja

--- intro_to_flask/app.py
+++ intro_to_flask/app.py
@@ -3,8 +3,71 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class GalileanMoons:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
+
+
 @app.route("/")
 def hello_world():
     return render_template(
         "jinja_intro.html", name="Bob Smith", template_name="Jinja2"
+    )
+
+
+@app.route("/expressions/")
+def expressions():
+
+    # interpolation
+    color = "brown"
+    animal_one = "fox"
+    animal_two = "dog"
+
+    #addition and subtraction
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 15
+
+    # string concatenation
+    first_name = "Captain"
+    last_name = "Marvel"
+
+    kwargs = {
+        "color": color,
+        "animal_one": animal_one,
+        "animal_two": animal_two,
+        "orange_amount": orange_amount,
+        "apple_amount": apple_amount,
+        "donate_amount": donate_amount,
+        "first_name": first_name,
+        "last_name": last_name
+    }
+
+    return render_template(
+        "expressions.html", **kwargs
+    )
+
+
+@app.route("/data-structures/")
+def render_data_structures():
+
+    movies = [
+        "Leon the Professtional",
+        "The Usual Suspects",
+        "A Beautiful Mind"
+    ]
+
+    car = {
+        "brand": "Tesla",
+        "model": "Roadster",
+        "year": "2020"
+    }
+
+    moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
+
+    return render_template(
+        "data_structures.html", movies=movies, car=car, moons=moons
     )
